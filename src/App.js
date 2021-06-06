f06d78c74a7281c680f2cc3a5ed278cf35811f46
Sathak Uzham <sathakhussam@gmail.com>
2021-06-06 16:02:06 +0530

    Added sidebar and colors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route, Link} from 'react-router-dom'
+
+// Pages
+import Dashboard from "./pages/dashboard/dashboard.page"
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/" component={null} />
-      </Switch>
+      <div className="sidebar">
+        <h1>Superior Rental</h1>
+        <div className="sidebar--links">
+          <Link to="/">Dashboard</Link>
+          <Link to="/">Orders</Link>
+          <Link to="/">Cars</Link>
+          <Link to="/">Users</Link>
+        </div>
+      </div>
+      <div className="content">
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+        </Switch>
+      </div>
     </div>
   );
 }
---
+++ src/pages/dashboard/dashboard.page.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './dashboard.styles.css'
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div className="Dashboard">
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
