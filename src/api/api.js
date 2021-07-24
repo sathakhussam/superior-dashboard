3265f971cbc8e1fa05642a32d018e8a7843f965d
Sathak Uzham <sathakhussam@gmail.com>
2021-07-24 14:11:38 -0700

    Changed api address

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://localhost:3000/api/v1/`
+  baseURL: `http://api.superiorrental.app/api/v1/`
   // baseURL: `https://api.superiorrental.app/api/v1/`
 });
 
