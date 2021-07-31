e9feabe84985b55a0128b7fe2ab25d697b8771e8
Sathak Uzham <sathakhussam@gmail.com>
2021-07-30 17:31:42 -0700

    Added https

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://api.superiorrental.app/api/v1/`
+  baseURL: `https://api.superiorrental.app/api/v1/`
   // baseURL: `https://api.superiorrental.app/api/v1/`
 });
 
