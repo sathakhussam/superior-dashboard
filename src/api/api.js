01abebf291f2b1684cbfb534e1630ea99aea24b4
Sathak Uzham <sathakhussam@gmail.com>
2021-08-23 10:44:04 +0530

    Changed ip

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://127.0.0.1:3000/api/v1/`
-  // baseURL: `https://api.superiorrental.app/api/v1/`
+  // baseURL: `http://127.0.0.1:3000/api/v1/`
+  baseURL: `https://api.superiorrental.app/api/v1/`
 });
 
 // api.defaults.headers.common["Referer"] = "dashboard.superiorrental.app"
